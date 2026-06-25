5f9fd97f9d5cfc04f302d36e087899dd46aa3d68
IshaAcharyaa <ishaacharya.com@gmail.com>
2026-06-25 18:06:42 +0545

    Information Card
    Make your Information card on java

---
+++ InfoCard.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class InfoCard {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Ask for details
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter your address: ");
+        String address = sc.nextLine();
+
+        System.out.print("Enter your phone number: ");
+        String phone = sc.nextLine();
+
+        System.out.print("Enter your email: ");
+        String email = sc.nextLine();
+
+        // Print the info card
+         System.out.println("---------Info Card-----------");
+        System.out.println("=============================");
+        System.out.println(" Name    : " + name);
+        System.out.println(" Address : " + address);
+        System.out.println(" Phone   : " + phone);
+        System.out.println(" Email   : " + email);
+        
+
+        sc.close();
+    }
+}
